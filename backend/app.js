2b1147d86782fe0804a8514f2eed9b9a4c1f666d
David Andres <dosoriocar@gmail.com>
2022-11-22 21:53:48 -0500

    backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import productsRoutes from './routes/products.routes.js';
 import clientsRoutes from './routes/clients.routes.js';
 import salesRoutes from './routes/sales.routes.js';
+import authRoutes from './routes/auth.routes.js';
 import CORS from 'Cors'
 
 
@@ -16,6 +17,7 @@ app.use(CORS());
 app.use(productsRoutes)
 app.use(clientsRoutes)
 app.use(salesRoutes)
+app.use(authRoutes)
 
 
 export default app;
